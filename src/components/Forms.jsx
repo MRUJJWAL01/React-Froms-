28ba6056b658401c634fcd92bcf4b98240a5741a
Ujjwal Singh Chouhan <your-email@example.com>
2025-06-23 01:44:51 +0530

    second commit

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
 const Forms = ({hendlFormSubmitData}) => {
-  const {register,handleSubmit,reset} = useForm();
+  const {register,handleSubmit,reset,formState:{errors}} = useForm();
   const hendleFormSumbit = data=> {
     hendlFormSubmitData(data);
     reset();
@@ -9,9 +9,12 @@ const Forms = ({hendlFormSubmitData}) => {
   return (
     <div className='mt-10 flex justify-center  items-center'>
       <form onSubmit={handleSubmit(hendleFormSumbit)} className='flex gap-5' action="">
-        <input  {...register("name")} placeholder='name'  className='px-2 py-1 rounded-md text-base outline-none text-md font-semibold bg-zinc-100' type="text" />
-        <input  {...register("email")}placeholder='email' className='px-2 py-1 rounded-md text-base outline-none text-md font-semibold bg-zinc-100'  type="text" />
-         <input {...register("image")} placeholder='image url' className='px-2 py-1 rounded-md text-base outline-none text-md font-semibold bg-zinc-100'  type="" />
+        <input  {...register("name",{required:"name is required"})} placeholder='name'  className='px-2 py-1 rounded-md text-base outline-none text-md font-semibold bg-zinc-100' type="text" />
+        {errors.name && <p>{errors.name.message}</p>}
+        <input  {...register("email",{required:"email is required"})}placeholder='email' className='px-2 py-1 rounded-md text-base outline-none text-md font-semibold bg-zinc-100'  type="email" />
+        {errors.email &&<p>{errors.email.message}</p>}
+         <input {...register("image",{required:"image is required"})} placeholder='image url' className='px-2 py-1 rounded-md text-base outline-none text-md font-semibold bg-zinc-100'  type="" />
+         {errors.image && <p>{errors.image.message}</p>}
         <input  className='px-2 py-1 rounded-md text-base outline-none text-md font-semibold text-white bg-blue-400' type="submit" />
       </form>
     </div>
